1340db572a2938e77ab69c9b1118c96367cc5262
sonyxme <bogdanova_02@mail.ru>
2022-09-19 10:06:13 +0300

    change DEBUG=False and create new def print

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a-b0nz5-xi6zx$inn@03)4&0&z^yjc8=c3#st(0c-f33u*&mo6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
